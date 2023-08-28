78eae6745822bd4d185723869f0d7e2dfe89ec67
SyedHussainSaber <syedhussainsaber8@gmail.com>
2023-08-28 22:32:41 +0530

    silder-1 made responsive

--- index.js
+++ index.js
@@ -2,6 +2,21 @@ const openMenu = document.querySelector(".open")
 const closeMenu = document.querySelector(".close")
 const mobileNav = document.querySelector(".mobile-nav")
 
+// script.js
+document.querySelectorAll('li a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute('href'));
+        if (target) {
+            window.scrollTo({
+                top: target.offsetTop,
+                behavior: 'smooth'
+            });
+        }
+    });
+});
+
 
 
 // const leftArrow = document.querySelector(".desktop-53-child1")
